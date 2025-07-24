38c68f6688b06fb0bfa6d9dd9e4a0a73be67a4d7
Vitor de Moraes <vitordemoraesfilho@gmail.com>
2025-07-24 18:07:40 -0300

    teste

--- API/vitalapi/Program.cs
+++ API/vitalapi/Program.cs
@@ -1,50 +1,6 @@
-using Microsoft.EntityFrameworkCore;
-using vitalapi.Context;
-
 var builder = WebApplication.CreateBuilder(args);
-
-// --- TESTE: DESATIVANDO OS CONTROLLERS ---
-// builder.Services.AddControllers(); // Linha também comentada
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
-
-// --- BLOCO DE BANCO DE DADOS AINDA DESATIVADO ---
-/*
-string connectionString;
-var dbHost = Environment.GetEnvironmentVariable("MYSQLHOST");
-
-if (string.IsNullOrEmpty(dbHost))
-{
-    connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
-}
-else
-{
-    var dbPassword = Environment.GetEnvironmentVariable("MYSQLPASSWORD");
-    var dbUser = Environment.GetEnvironmentVariable("MYSQLUSER");
-    var dbPort = Environment.GetEnvironmentVariable("MYSQLPORT");
-    var dbName = Environment.GetEnvironmentVariable("MYSQLDATABASE");
-    connectionString = $"Server={dbHost};Port={dbPort};Database={dbName};Uid={dbUser};Pwd={dbPassword};";
-}
-
-var serverVersion = new MySqlServerVersion(new Version(8, 0, 32));
-
-builder.Services.AddDbContext<vitalcontext>(options =>
-    options.UseMySql(connectionString, serverVersion)
-);
-*/
-// --- FIM DO BLOCO DESATIVADO ---
-
-
 var app = builder.Build();
 
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.MapGet("/", () => "Olá, Mundo! A API está no ar!");
 
-app.UseHttpsRedirection();
-app.UseAuthorization();
-app.MapControllers();
 app.Run();
