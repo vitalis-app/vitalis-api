287a213d1106839c6c9517c1806f22f38820dc85
Vitor de Moraes <vitordemoraesfilho@gmail.com>
2025-07-24 17:59:44 -0300

    teste api

--- API/vitalapi/Program.cs
+++ API/vitalapi/Program.cs
@@ -8,33 +8,31 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 
-
+// --- BLOCO DE BANCO DE DADOS TEMPORARIAMENTE DESATIVADO ---
+/*
 string connectionString;
-
 var dbHost = Environment.GetEnvironmentVariable("MYSQLHOST");
 
 if (string.IsNullOrEmpty(dbHost))
 {
-
     connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 }
 else
 {
-
     var dbPassword = Environment.GetEnvironmentVariable("MYSQLPASSWORD");
     var dbUser = Environment.GetEnvironmentVariable("MYSQLUSER");
     var dbPort = Environment.GetEnvironmentVariable("MYSQLPORT");
     var dbName = Environment.GetEnvironmentVariable("MYSQLDATABASE");
-
     connectionString = $"Server={dbHost};Port={dbPort};Database={dbName};Uid={dbUser};Pwd={dbPassword};";
 }
-
 
 var serverVersion = new MySqlServerVersion(new Version(8, 0, 32));
 
 builder.Services.AddDbContext<vitalcontext>(options =>
     options.UseMySql(connectionString, serverVersion)
 );
+*/
+// --- FIM DO BLOCO DESATIVADO ---
 
 
 var app = builder.Build();
@@ -46,9 +44,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
