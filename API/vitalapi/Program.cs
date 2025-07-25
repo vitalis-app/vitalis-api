0b6083152c1b1b9311ebde3eb2a14e43f53da1c2
Vitor de Moraes <vitordemoraesfilho@gmail.com>
2025-07-25 14:18:52 -0300

    protegido swagger

--- API/vitalapi/Program.cs
+++ API/vitalapi/Program.cs
@@ -34,10 +34,59 @@ builder.Services.AddDbContext<vitalcontext>(options =>
 
 var app = builder.Build();
 
-
+if (app.Environment.IsDevelopment())
+{
     app.UseSwagger();
     app.UseSwaggerUI();
+}
 
+app.MapGet("/healthz", () => Results.Ok("Healthy"));
+
+app.UseAuthorization();
+app.MapControllers();
+
+app.Run();
+using Microsoft.EntityFrameworkCore;
+using vitalapi.Context;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
+builder.WebHost.UseUrls($"http://*:{port}");
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+string connectionString;
+var dbHost = Environment.GetEnvironmentVariable("MYSQLHOST");
+
+if (string.IsNullOrEmpty(dbHost))
+{
+    connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+}
+else
+{
+    var dbPassword = Environment.GetEnvironmentVariable("MYSQLPASSWORD");
+    var dbUser = Environment.GetEnvironmentVariable("MYSQLUSER");
+    var dbPort = Environment.GetEnvironmentVariable("MYSQLPORT");
+    var dbName = Environment.GetEnvironmentVariable("MYSQLDATABASE");
+    connectionString = $"Server={dbHost};Port={dbPort};Database={dbName};Uid={dbUser};Pwd={dbPassword};";
+}
+
+var serverVersion = new MySqlServerVersion(new Version(8, 0, 32));
+
+builder.Services.AddDbContext<vitalcontext>(options =>
+    options.UseMySql(connectionString, serverVersion)
+);
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.MapGet("/healthz", () => Results.Ok("Healthy"));
 
