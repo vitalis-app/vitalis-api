6829cd2560f01dda21f420d172466c3be5271f27
cauetrainotti <trainotti.caue@gmail.com>
2025-09-04 16:49:47 -0300

    Alterações básicas UsuarioDTO

--- API/vitalapi/DTO'S/UsuarioDto.cs
+++ API/vitalapi/DTO'S/UsuarioDto.cs
@@ -1,4 +1,6 @@
-﻿namespace vitalapi.DTO_S
+﻿using vitalapi.Models.Usuario;
+
+namespace vitalapi.DTO_S
 {
     public class UsuarioDto
     {
@@ -10,9 +12,13 @@
         public DateTime DataNascimento { get; set; }
         public string Genero { get; set; }
         public string FotoPerfil { get; set; }
-        public string PlanoId { get; set; }
         public bool Ativo { get; set; }
         public DateTime DataCriacao { get; set; }
+
+        public UsuarioProgresso Progresso { get; set; }
+        public int AssinaturaAtivaId { get; set; }
+        public UsuarioConfiguracao Configuracoes { get; set; }
+
     }
     public class CreateUsuarioDto
     {
@@ -23,11 +29,13 @@
         public DateTime DataNascimento { get; set; }
         public string Genero { get; set; }
         public string FotoPerfil { get; set; }
-        public string PlanoId { get; set; }
+
+        public int AssinaturaAtivaId { get; set; }
     }
     public class UpdateUsuarioDto
     {
         public string Nome { get; set; }
+        public string Senha { get; set; }
         public string Telefone { get; set; }
         public string FotoPerfil { get; set; }
         public string PlanoId { get; set; }
