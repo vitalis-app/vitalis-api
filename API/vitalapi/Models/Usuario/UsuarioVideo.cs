fb4317ad5fe6dde49c7feb12378b6bf71c6ef6c3
cauetrainotti <trainotti.caue@gmail.com>
2025-09-05 22:01:34 -0300

    Alterações simples

---
+++ API/vitalapi/Models/Usuario/UsuarioVideo.cs
@@ -0,0 +1,13 @@
+﻿namespace vitalapi.Models.Usuario
+{
+    public class UsuarioVideo
+    {
+        public int UsuarioId { get; set; }
+        public Usuario Usuario { get; set; }
+
+        public int VideoId { get; set; }
+        public Video Video { get; set; }
+
+        public DateTime DataAssistido { get; set; }
+    }
+}
