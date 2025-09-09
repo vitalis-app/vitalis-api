987fbbccff12ea697126bc180fc5585727fc38a7
Vitor de Moraes <vitordemoraesfilho@gmail.com>
2025-09-09 00:44:53 -0300

    swagger aberto

--- API/vitalapi/Program.cs
+++ API/vitalapi/Program.cs
@@ -35,11 +35,9 @@ builder.Services.AddDbContext<VitalContext>(options =>
 var app = builder.Build();
 
 
-if (app.Environment.IsDevelopment())
-{
     app.UseSwagger();
     app.UseSwaggerUI();
-}
+
 
 
 app.MapGet("/healthz", () => Results.Ok("Healthy"));
